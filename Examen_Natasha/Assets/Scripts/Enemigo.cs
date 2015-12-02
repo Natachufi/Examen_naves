635b383a08b694686cd5df0dc9c67583b70b88b3
Natachufi <natashamolins@gmail.com>
2015-12-02 19:43:48 +0100

    Sistema de particulas y destructores

--- Examen_Natasha/Assets/Scripts/Enemigo.cs
+++ Examen_Natasha/Assets/Scripts/Enemigo.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Enemigo : MonoBehaviour {
 	Rigidbody2D rg;
 	public float fuerza_enemigo=1f;
+	public GameObject efecto;
 	// Use this for initialization
 	void Start () {
 		rg = GetComponent<Rigidbody2D> ();
@@ -17,6 +18,8 @@ public class Enemigo : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D objeto){
 		//Debug.Log (objeto.transform.name);
 		if (objeto.transform.tag == "laser") {
+			GameObject e =(GameObject)Instantiate(efecto,transform.position, transform.rotation);
+			Destroy (e,0.9f);
 			Destroy (gameObject);
 		}
 		if (objeto.transform.tag == "destructor") {
